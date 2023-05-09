fbd93cedff5ec777f7b0de8428d3f2b9ca608b23
Gabriel Santos <sgtux@protonmail.com>
2023-05-09 07:04:12 -0300

    Py algorithms

---
+++ py-algorithms/main.py
@@ -0,0 +1,4 @@
+from sort import bubbleSort
+arr = [9, 3, 5, 1, 2, 4, 5, 7, 8, 8, 1]
+bubbleSort(arr)
+print(arr)
---
+++ py-algorithms/sort.py
@@ -0,0 +1,7 @@
+def bubbleSort(arr: list):
+    for i in arr:
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
